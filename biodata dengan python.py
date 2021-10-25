f36a0c27c7d01e9971c3c61a2490d172031c83b6
helmianuar213 <92745457+helmianuar213@users.noreply.github.com>
2021-10-25 18:55:06 +0700

    Add files via upload

---
+++ biodata dengan python.py
@@ -0,0 +1,25 @@
+#!/usr/bin/pyhton
+
+print "==========="
+print "  Biodata  "
+print "==========="
+#Variabel
+nama= raw_input ("Masukan Nama Anda: ")
+alamat= raw_input ("Masukan Alamat: ")
+TTL= raw_input ("Masukan Tempat Tanggal lahir Anda: ")
+jk= raw_input ("Jenis Kelamin: ")
+agama= raw_input ("Masukan Agama: ")
+pekerjaan= raw_input ("Pekerjaan: ")
+hoby= raw_input ("Masukan Hoby: ")
+
+#Menampilkan input user]
+print "==========="
+print "  Biodata  "
+print "==========="
+print "Nama: ",namap
+print "Alamat: ",Alamat
+print "Tempat Tanggal lahir: ",TTL
+print "Jenis Kelamin: ",jk
+print "agama: ",agama
+print "pekerjaan: ",Pekerjaan
+print "hoby: ",hoby
