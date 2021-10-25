638afa620d0f47e47dd75acb16f8f17cca989f04
helmianuar213 <92745457+helmianuar213@users.noreply.github.com>
2021-10-25 21:28:11 +0700

    Add files via upload

---
+++ biodata python.py
@@ -0,0 +1,23 @@
+print("Selmat datang di Universitas Pelita Bangsa Kota Bekasi")
+print("Silahkan Isi biodata anda")
+
+
+nama_lengkap = input("Masukan Nama Lengkap : " )
+nama_panggilan = input("Nama Panggilan : ")
+NPM = input("NPM : ")
+tanggal_lahir = input("Tempat,Tanggal Lahir : ")
+umur = input("Umur : ")
+telepon = input ("Telepon : ") 
+Alamat = input ("Alamat : ") 
+
+print ()
+
+
+print("Assalamual'aikum.")
+print ()
+print("Let me intoduce my self. Myname is" ,nama_lengkap , "but you can call me",nama_panggilan,"."
+    ,"My NPM is",NPM,".I was born in",tanggal_lahir,"and iam",umur,"years old",
+    ".I am very glat if you want to invite my house in",Alamat,"So,dont forget to call me before with teh nomber",telepon,".")
+print ()
+print("Thank You.")
+
